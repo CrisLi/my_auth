58f4bdd687abdc6b916459e59d72f702c82d467b
Chris Li <chrisandkitty@163.com>
2018-01-24 15:49:38 +0800

    Customize jwt payload

--- src/hooks/auth.hooks.js
+++ src/hooks/auth.hooks.js
@@ -5,7 +5,16 @@ module.exports = {
   before: {
     create: [
       userIdentifier,
-      auth.hooks.authenticate(['local', 'jwt'])
+      auth.hooks.authenticate(['local']),
+      (context) => {
+        const { params, params: { user = {} } } = context;
+        params.payload = {
+          id: user['_id'],
+          username: user.username,
+          org: user.org,
+          roles: user.roles
+        };
+      }
     ]
   }
 };
